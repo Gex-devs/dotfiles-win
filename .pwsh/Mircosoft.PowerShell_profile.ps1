c24ae9f8815189509405bd54e7e576838b8849cf
dgexi <dgexi@GEX-DESKTOP>
2024-01-12 00:22:30 +0100

    init config

---
+++ .pwsh/Mircosoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+﻿
+$ENV:STARSHIP_CONFIG= Join-Path $env:HOMEPATH ".config\starship.toml"
+#$ENV:STARSHIP_DISTRO="??? Gex"
+$myCode= Join-Path $env:HOMEPATH "\Documents\Documents\code"
+set-alias bigcode "D:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+set-alias touch New-Item
+Invoke-Expression (&starship init powershell)
+
